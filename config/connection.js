edc394af5029c4878d6d6fb4ba1ed1d7e5bf7ec0
Junnel Padilla <junnelpadilla307@gmail.com>
2023-06-27 23:33:36 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize (
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // If running on Heroku, use the JawsDB MySQL database
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // If running locally, use your local MySQL database
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        port: 3306,
+      host: 'ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      dialect: 'mysql',
+      database: 'c153m86emzv8xuoh',
+      port: 3306
     }
-)
+  );
+}
 
 module.exports = sequelize;
